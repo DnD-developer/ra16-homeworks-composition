f7aea47e1fc665138a70249cd09d67444f70234c
DND-dev <dev.work@dnd-projects.ru>
2023-05-14 17:46:48 +0300

    create decomposition

---
+++ src/components/Yandex-component/Yandex.jsx
@@ -0,0 +1,33 @@
+// Отвечает за весь Компонент поиска яндекса
+
+// принимает в качестве детей:
+//Widget для новостей:
+//    List
+//       item-news
+//Widget работы над ошибками:
+//    item-light
+//      item-main-text
+//Search
+//    Navigation
+//       List
+//         item-navigatiion
+//    Logo
+//    String-seach
+//    Help
+//      item-main-text
+//      item-subtitle
+//    Comercial
+//Widget погоды
+//  Weather
+//Widget карты германии
+//  item-light
+//     item-main-text
+//Widget телепрограммы
+//   item-tv
+//      item-time
+//      item-main-text
+//      item-subtitle
+//Widget Эфир
+//   item-tv
+//     item-main-text
+//     item-subtitle
